cef969dfb6c773642cffc14b7ca3633e7e94006c
jjwall <jacob.wallace@utexas.edu>
2024-05-31 18:48:44 -0500

    fishing state added for player char. Just fish south implemented for now

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -12,7 +12,10 @@ func _ready():
 func _always_process(_delta):
 	direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
 	
-	if direction == Vector2(0,0):
+	if Input.is_action_just_pressed("ui_accept"):
+		_goto("fish")
+	
+	if direction == Vector2(0,0) and _current_state != "fish":
 		_goto("idle")
 	else:
 		_goto("walk")
@@ -21,6 +24,11 @@ func _always_physics_process(_delta):
 	velocity = direction * SPEED
 	
 	move_and_slide()
+
+#region State fish
+func _state_fish_enter():
+	# match direction ...
+	anim.play("fish_south")
 
 #region State idle
 func _state_idle_enter():
