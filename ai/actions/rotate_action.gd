06ce94ee9e8fe62c8b614b861e6bdc5e685b29bd
jjwall <jacob.wallace@utexas.edu>
2024-06-01 17:59:05 -0500

    fish behavior successfully started :D

---
+++ ai/actions/rotate_action.gd
@@ -0,0 +1,22 @@
+extends ActionLeaf
+
+@export var rotation_speed = 3
+
+func tick(actor, blackboard):
+	var target_rotation = blackboard.get_value("new_angle")
+	var delta = get_physics_process_delta_time()
+	
+	# Calculate the shortest path to the target rotation
+	var shortest_angle = angle_difference(actor.rotation, target_rotation)
+
+	# Calculate the amount to rotate this frame
+	var angle_step = rotation_speed * delta
+
+	# If the angle to rotate is smaller than the step, set the final rotation
+	if abs(shortest_angle) <= angle_step:
+		actor.rotation = target_rotation
+		return SUCCESS
+	else:
+		# Otherwise, rotate in the direction of the target
+		actor.rotation += angle_step * sign(shortest_angle)
+		return RUNNING
