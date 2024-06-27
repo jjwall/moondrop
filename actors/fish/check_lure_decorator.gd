0002f3ca576b261db136154213415c9675713fd8
jjwall <jacob.wallace@utexas.edu>
2024-06-26 23:07:07 -0500

    nibble sequence now triggers runaway sequence if at any point a lure is not detected during the nibbling sequence (i.e. a lure gets pulled too early)

--- actors/fish/check_lure_decorator.gd
+++
@@ -1,8 +0,0 @@
-class_name CheckLureDecorator extends Decorator
-
-# This decorator checks to ensure there are still lures detected by the fish.
-func tick(actor: Node, blackboard: Blackboard) -> int:
-	if blackboard.get_value("lure_objects").size() == 0:
-		return FAILURE
-	else:
-		return SUCCESS
