726b1637c5a1435c8428fede7514335f547eaea2
jjwall <jacob.wallace@utexas.edu>
2024-06-30 14:46:15 -0500

    lure animations for catching, nibbled, and bit

---
+++ objects/lure/lure.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+func play_bit_anim():
+	$AnimatedSprite2D.play("bit")
+
+func play_nibbled_anim():
+	$AnimatedSprite2D.play("nibbled")
+
+func play_catching_anim():
+	$AnimatedSprite2D.play("catching")
