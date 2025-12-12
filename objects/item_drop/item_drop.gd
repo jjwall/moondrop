d5c5483f9cbf14a6e449c3ba1ec9bc4af1f26204
Jacob <jacob.wallace@utexas.edu>
2025-12-12 13:41:25 -0600

    bouncing animation when they drop works

--- objects/item_drop/item_drop.gd
+++ objects/item_drop/item_drop.gd
@@ -4,6 +4,14 @@ var place_holder_item_scene = preload("res://objects/fish_types/clown_fish/clown
 var recent_manual_drop = false
 var item_data_ref = null
 
+@onready var host_sprite
+
+var drop_height = 25
+var starts_at_top = false
+
+const TWEEN_DURATION = 0.1
+const RECOVERY_FACTOR = 0.52
+
 func set_item_data(item_data):
 	item_data_ref = item_data
 
@@ -16,9 +24,10 @@ func _ready() -> void:
 		}
 		
 	var item = item_data_ref.scene.instantiate()
+	host_sprite = item.get_node("AnimatedSprite2D") # maybe check for Srpite2D as well
 	#item.global_position = self.global_position
 	add_child(item)
-	# TODO: Add dropping animation
+	start_bounce_tween()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"): # and not recent_manual_drop:
@@ -31,3 +40,30 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 				self.queue_free()
 		else:
 			recent_manual_drop = false
+
+func start_bounce_tween():
+	if starts_at_top:
+		await drop_down(drop_height).finished
+	
+	var bounce_height = drop_height * RECOVERY_FACTOR
+	
+	while bounce_height > 1:
+		await bounce_up(bounce_height).finished
+		await drop_down(drop_height).finished
+		bounce_height = bounce_height * RECOVERY_FACTOR
+	
+func bounce_up(height) -> Tween:
+	var y_end = -height
+	var tween = create_tween()
+	tween.tween_property(host_sprite, "position:y", y_end, TWEEN_DURATION)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.play()
+	return tween
+
+func drop_down(height) -> Tween:
+	#var y_start = -height
+	var tween = create_tween()
+	tween.tween_property(host_sprite, "position:y", 0, TWEEN_DURATION)
+	tween.set_ease(Tween.EASE_IN)
+	tween.play()
+	return tween
